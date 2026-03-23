f20bef4b5e5046377954e3f2186abd21fe4ee32c
Christian Fickinger <mail@christianfickinger.de>
2026-03-23 18:02:38 +0100

    v1.0.0 — Kirby 5 compatible, new settings format
    - Bump version to 1.0.0

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 pwConfig::register('pwheading', __DIR__ . '/src/config');
 
 Kirby::plugin('kirbydesk/kirbyblock-heading', [
-
 	/* -------------- Extensions --------------*/
 	'blueprints' => require_once 'src/extensions/blueprints.php',
 	'snippets' => require_once 'src/extensions/snippets.php',
